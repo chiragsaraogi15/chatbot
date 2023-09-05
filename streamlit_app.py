e2baf0f36a4a9e184141aa023985ad834116b869
Chirag Saraogi <chiragsaraogi15@gmail.com>
2023-09-05 17:21:28 -0400

    Add files via upload

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,16 @@
 import streamlit as st
+import os
+from langchain.document_loaders import UnstructuredURLLoader
+from langchain.text_splitter import CharacterTextSplitter
+import pickle
+import faiss
+from langchain.vectorstores import FAISS
+from langchain.embeddings import OpenAIEmbeddings
+import pickle
+from langchain.chains import RetrievalQAWithSourcesChain
+from langchain.chains.question_answering import load_qa_chain
+from langchain import OpenAI
+
 
 # Streamlit UI
 st.title("WebChatMate: Your Conversational URL Companion")
