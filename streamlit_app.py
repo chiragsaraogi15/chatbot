6027018cd36909ec8afaee7296667c89fd1a0dfc
Chirag Saraogi <chiragsaraogi15@gmail.com>
2023-09-06 11:11:27 -0400

    Add files via upload

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,21 +1,34 @@
-import streamlit as st
 import os
-from langchain.document_loaders import UnstructuredURLLoader
-from langchain.text_splitter import CharacterTextSplitter
-import pickle
-import faiss
-from langchain.vectorstores import FAISS
-from langchain.embeddings import OpenAIEmbeddings
-import pickle
-from langchain.chains import RetrievalQAWithSourcesChain
-from langchain.chains.question_answering import load_qa_chain
-from langchain import OpenAI
-from process_save_doc import process_and_save
 
-# Initialize session state
-st.session_state.url_list = []
-st.session_state.VectorStore = None
-st.session_state.chain = None
+import streamlit as st
+from langchain import OpenAI
+from langchain.chains import RetrievalQAWithSourcesChain
+from langchain.document_loaders import UnstructuredURLLoader
+from langchain.embeddings import OpenAIEmbeddings
+from langchain.text_splitter import CharacterTextSplitter
+from langchain.vectorstores import FAISS
+
+
+def process_and_save(urls):
+    loaders = UnstructuredURLLoader(urls=urls)
+    data = loaders.load()
+
+    text_splitter = CharacterTextSplitter(
+        separator="\n", chunk_size=1000, chunk_overlap=300
+    )
+    docs = text_splitter.split_documents(data)
+
+    embeddings = OpenAIEmbeddings()
+    vectorStore_openAI = FAISS.from_documents(docs, embeddings)
+
+    return vectorStore_openAI
+
+
+if "url_list" not in st.session_state:
+    # Initialize session state
+    st.session_state.url_list = []
+    st.session_state.VectorStore = None
+    st.session_state.chain = None
 
 st.title("WebChatMate")
 st.subheader("Your Conversational URL Companion")
@@ -24,28 +37,29 @@ os.environ["OPENAI_API_KEY"] = st.secrets["OPENAI_KEY"]
 
 url = st.text_input("Enter Your URL")
 
-submit = st.button('Submit')
+submit = st.button("Submit")
 
 if submit and url:
-    
-    st.session_state.url_list.append(url)
+    if url not in st.session_state.url_list:
+        st.session_state.url_list += [url]
     st.session_state.VectorStore = process_and_save(st.session_state.url_list)
     llm = OpenAI(temperature=0)
-    st.session_state.chain = RetrievalQAWithSourcesChain.from_llm(llm=llm, retriever=st.session_state.VectorStore.as_retriever())
+    st.session_state.chain = RetrievalQAWithSourcesChain.from_llm(
+        llm=llm, retriever=st.session_state.VectorStore.as_retriever()
+    )
 
+st.write("URLs", st.session_state.url_list)
+
+if st.session_state.chain:
     user_question = st.text_input("Enter your question:")
 
-    ask = st.button('Ask')
-    
+    ask = st.button("Ask")
+
     if ask and user_question and st.session_state.chain:
-    
-        response = st.session_state.chain({"question": user_question}, return_only_outputs=True)
-        answer = response['answer'].replace('\n', '')
-        sources = response.get('sources', '')
+        response = st.session_state.chain(
+            {"question": user_question}, return_only_outputs=True
+        )
+        answer = response["answer"].replace("\n", "")
+        sources = response.get("sources", "")
         st.write("Answer:", answer)
         st.write("Sources:", sources)
-            
- 
-            
-
-
